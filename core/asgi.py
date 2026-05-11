0352d9c424986804153b0cebc39efa9be091036c
Princeton Afeez <princetonafeez@gmail.com>
2026-05-11 14:14:51 -0700

    ASGI config for Above Board

---
+++ core/asgi.py
@@ -0,0 +1,11 @@
+"""ASGI config for Above Board."""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
+
+application = get_asgi_application()
+
